f0d2d5ad27546e1ce912780c232ea2e76e852a49
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-02-18 10:25:25 +0000

    add authentication helpers

--- app/authentication/auth.py
+++ app/authentication/auth.py
@@ -1,23 +1,16 @@
-import os
 from datetime import UTC, datetime, timedelta
 
 import jwt
-from dotenv import load_dotenv
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, Request, status
 from fastapi.security import OAuth2PasswordBearer
 from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
+from ..config import settings
 from ..db import database
 from ..models.user_model import User
 from ..schemas.user_schema import TokenData
-
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
@@ -47,28 +40,33 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
         expire = datetime.now(UTC) + timedelta(minutes=20)
 
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
 
     return encoded_jwt
 
 
 def get_current_user(
-    token: str = Depends(oauth2_schema), db: Session = Depends(database.get_db)
+    request: Request,
+    db: Session = Depends(database.get_db),
 ):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credential",
-        headers={"WWW-Authenticate": "Bearer"},
+        headers={"X-Authentication-Error": "Cookie not found or invalid"},
     )
-
+    token = request.cookies.get("access_token")
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
-    except InvalidTokenError:
-        raise credentials_exception
+    except InvalidTokenError as err:
+        raise credentials_exception from err
 
     user = db.query(User).filter(User.username == token_data.username).first()
     if user is None:
