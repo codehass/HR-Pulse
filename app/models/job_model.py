8c0c6091789940a73bec16e0e5160a1c886e8a29
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-02-26 14:30:43 +0000

    create job model

---
+++ app/models/job_model.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from ..db.database import Base
+
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    revenue = Column(Integer)
+    years_exp = Column(Integer)
+    location = Column(String(100))
+    ownership = Column(String(100))
+    sector = Column(String(100))
+    job_description_text = Column(Text)
+    predicted_salary = Column(Float)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="jobs")
