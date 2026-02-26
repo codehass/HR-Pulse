dd1bc1d3522d2b521fa2ddbd99101860a9df40b8
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-02-26 14:31:02 +0000

    add relationship to User model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from ..db.database import Base
 
@@ -11,3 +12,4 @@ class User(Base):
     email = Column(String(100), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
+    jobs = relationship("Job", back_populates="owner")
