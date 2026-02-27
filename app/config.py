9ac7bf47f69c73f971c1b17424288652cf2354f0
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-02-27 23:11:55 +0000

    edit docker compose

--- app/config.py
+++ app/config.py
@@ -3,18 +3,12 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_HOST: str = Field(...)
-    DATABASE_PASSWORD: str = Field(...)
-    DATABASE_NAME: str = Field(...)
-    DATABASE_USER: str = Field(...)
-    DATABASE_PORT: int = Field(...)
     SECRET_KEY: str = Field(...)
     ALGORITHM: str = Field(...)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(...)
     FRONTEND_URL: str = Field(...)
     GOOGLE_API_KEY: str = Field(...)
     HF_TOKEN: str = Field(...)
-    MLFLOW_TRACKING_URI: str = Field(...)
     DATABASE_URL: str = Field(...)
     ENDPOINT: str = Field(...)
     KEY: str = Field(...)
